7427c758d7b375c87e4e448f6d2de2641b07c005
EC2 Default User <ec2-user@ip-172-31-31-33.ec2.internal>
2022-10-16 13:56:39 +0000

    customer migrateできない

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -6,11 +6,24 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      #flash[:success] = "メールアドレスを更新しました"
       redirect_to admin_items_path
     else
       render :new
     end
+  end
+  def index
+    @items = Item.all
+  end
+  def show
+  @item = Item.find(params[:id])
+  end
+  def edit
+     @item = Item.find(params[:id])
+  end
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to admin_item_path(item)
   end
   def item_params
     params.require(:item).permit(:name, :introduction, :price, :is_active, :image)
--- db/migrate/20221011124716_create_items.rb
+++ db/migrate/20221011124716_create_items.rb
@@ -1,11 +1,11 @@
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
-      
+
       t.string :name
       t.text :introduction
       t.integer :price
-      t.boolean :is_active
+      t.boolean :is_active, null: false, default: 0
 
       t.timestamps
     end
