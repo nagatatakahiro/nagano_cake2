44d6acaa3d4899d42bfdb0f355e927ac67b08af5
EC2 Default User <ec2-user@ip-172-31-31-33.ec2.internal>
2022-11-03 01:14:58 +0000

    退会機能つける

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -4,18 +4,18 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
-    @customer = customer.find(params[:id])
+    @customer = Customer.find(params[:id])
   end
 
   def edit
-    @customer = customer.find(params[:id])
+    @customer = Customer.find(params[:id])
   end
   def update
-    customer = customer.find(params[:id])
-    customer.update(customer_params)
+    customer = Customer.find(params[:id])
+    Customer.update(customer_params)
     redirect_to admin_customer_path(customer)
   end
   def customer_params
-    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :is_deleted, :telephone_number, :email, :password)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :is_deleted, :telephone_number, :email, :password,:is_deleted)
   end
 end
--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,9 +1,17 @@
 class Public::CustomersController < ApplicationController
   def show
-    @User = current_customer
+    @customer = current_customer
   end
 
   def edit
-    @User = current_customer
+    @customer = current_customer
+  end
+  def update
+    customer = Customer.find(params[:id])
+    Customer.update(customer_params)
+    redirect_to admin_customer_path(customer)
+  end
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :is_deleted, :telephone_number, :email, :password)
   end
 end
